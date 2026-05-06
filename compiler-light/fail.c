a29912b0b591ad337ecd037a0a072e533cb00c7e
EduardoGMora <lalogomo2000@outlook.com>
2026-05-06 14:14:16 -0600

    Add Bison parser header file for compiler-light
    This commit introduces a new header file `parser.tab.h` generated by Bison. It defines the token types and value types used in the parser, along with necessary preprocessor directives and licensing information. This file is essential for the integration of the Bison parser into the compiler-light project.

--- compiler-light/fail.c
+++ compiler-light/fail.c
@@ -1,16 +1,8 @@
-/* wrong-test-2.c
-   Errors covered:
-   - Missing semicolon in global declaration
-   - Invalid condition in while (empty)
-   - Missing closing brace in function body
-   - Invalid return expression
-*/
-
 #include <stdio.h>
 
 #define LIMIT 50
 
-int counter       /* missing semicolon */
+int counter       
 
 int decrement(int n) {
     n = n - 1;
@@ -21,13 +13,13 @@ int helper(int a, int b) {
     int diff;
     diff = a - b;
 
-    while () {        /* empty condition */
+    while () {        
         diff = diff - 1;
     }
 
-    return diff + ;   /* incomplete expression */
+    return diff + ;   
 
-                      /* missing closing brace */
+                      
 
 int main() {
     int x;
