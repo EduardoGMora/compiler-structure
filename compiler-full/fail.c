f9162e29c7e8e471887e92bb3aff2f54ff747d60
EduardoGMora <lalogomo2000@outlook.com>
2026-05-26 16:07:13 -0600

    Add initial implementation of test files and lexer, parser, and error reporting for a new full compiler

---
+++ compiler-full/fail.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define LIMIT 50
+
+int counter       
+
+int decrement(int n) {
+    n = n - 1;
+    return n;
+}
+
+int helper(int a, int b) {
+    int diff;
+    diff = a - b;
+
+    while () {        
+        diff = diff - 1;
+    }
+
+    return diff + ;   
+
+                      
+
+int main() {
+    int x;
+    x = 10;
+
+    while (x > 0) {
+        x = decrement(x);
+    }
+
+    return x;
+}
